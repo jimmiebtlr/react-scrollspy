396958a068c341b19922359f4c6b4f8c459e1a14
Jimmie <jimmiebtlr@gmail.com>
2016-01-20 06:52:03 -0600

    Fix some boilerplate.

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,5 +1,5 @@
 echo "> Start transpiling ES2015"
 echo ""
-./node_modules/.bin/babel --presets es2015,react --plugins "transform-runtime" lib --ignore __tests__ --out-dir ./dist
+./node_modules/.bin/babel --presets es2015,react --plugins "transform-runtime" lib --out-dir ./dist
 echo ""
 echo "> Complete transpiling ES2015"
